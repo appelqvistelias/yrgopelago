1100ed425d6559e49c0d747b14c2a8f2edf445b9
Elias Appelqvist <eliasappelqvist@gmail.com>
2024-12-10 10:19:19 +0100

    added some minor content and structure

--- index.php
+++ index.php
@@ -3,6 +3,19 @@ require_once(__DIR__ . '/header.php');
 ?>
 
 <main>
+    <div class="inner-wrapper">
+        <section id="hero">
+            <h1>Welcome to the wonderful hotel HOTEL-NAME on the island of ISLAND-NAME</h1>
+        </section>
+
+        <section id="info">
+
+        </section>
+
+        <section id="booking">
+
+        </section>
+    </div> <!-- .inner-wrapper -->
 
 </main>
 
