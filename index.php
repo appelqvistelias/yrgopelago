3e2ef404acd50959baada6e4f9237449751139ce
Elias Appelqvist <eliasappelqvist@gmail.com>
2024-12-12 15:37:47 +0100

    added transfercode validation function

--- index.php
+++ index.php
@@ -35,6 +35,17 @@ require_once(__DIR__ . '/header.php');
             $totalCost = 4;
         }
 
+        // See if transferCode is valid
+        function isValidUuid(string $uuid): bool
+        {
+
+            if (!is_string($uuid) || (preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $uuid) !== 1)) {
+                return false;
+            }
+
+            return true;
+        }
+
         // if (isset($transferCode, $totalCost)) {
         //     $transferData = json_decode(file_get_contents(__DIR__ . '/guests/guests.json'), true);
 
