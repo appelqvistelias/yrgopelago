fc1bcd9ff737ea6575b3b5ad6c901a47baa46d88
Elias Appelqvist <eliasappelqvist@gmail.com>
2024-12-17 12:55:16 +0100

    added transfercode format verification function

--- functions.php
+++ functions.php
@@ -1,3 +1,14 @@
 <?php
 
 declare(strict_types=1);
+
+// See if transferCode is valid
+function isValidUuid(string $uuid): bool
+{
+
+    if (!is_string($uuid) || (preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $uuid) !== 1)) {
+        return false;
+    }
+
+    return true;
+}
