c63e0b04320d07daca89b4653a54c37313433591
Elias Appelqvist <eliasappelqvist@gmail.com>
2024-12-18 17:25:38 +0100

    removed require to deleted file

--- init.php
+++ init.php
@@ -5,6 +5,5 @@ declare(strict_types=1);
 session_start();
 
 require_once __DIR__ . "/vendor/autoload.php";
-require_once __DIR__ . "/database.php";
 require_once __DIR__ . "/functions.php";
 require_once __DIR__ . "/booking.php";
