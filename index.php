31a4edb87ed29ea9860569782b4a33aba44db08c
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-01-02 16:37:45 +0100

    added some styling to booking section

--- index.php
+++ index.php
@@ -57,84 +57,88 @@ require_once __DIR__ . '/header.php';
 
     <section id="booking">
         <div class="inner-wrapper">
-            <div class="calendar-container">
-                <h3>January 2025</h3>
-                <div class="calendar-grid">
-                    <div class="weekday">Mon</div>
-                    <div class="weekday">Tue</div>
-                    <div class="weekday">Wed</div>
-                    <div class="weekday">Thu</div>
-                    <div class="weekday">Fri</div>
-                    <div class="weekday">Sat</div>
-                    <div class="weekday">Sun</div>
-                </div> <!-- .calendar-grid -->
-            </div> <!-- .calendar-container -->
+            <div class="booking-grid">
+                <div class="calendar-container">
+                    <h3>January 2025</h3>
+                    <div class="calendar-grid">
+                        <div class="weekday">Mon</div>
+                        <div class="weekday">Tue</div>
+                        <div class="weekday">Wed</div>
+                        <div class="weekday">Thu</div>
+                        <div class="weekday">Fri</div>
+                        <div class="weekday">Sat</div>
+                        <div class="weekday">Sun</div>
+                    </div> <!-- .calendar-grid -->
+                </div> <!-- .calendar-container -->
 
-            <form id="booking-form" method="post" action="index.php">
-                <div class="arrival-departure">
-                    <label for="startdate">Arrival:</label>
-                    <input type="date" id="startdate" name="startdate" min="2025-01-01" max="2025-01-31" required>
+                <div class="booking-form-container">
+                    <form id="booking-form" method="post" action="index.php">
+                        <div class="arrival-departure">
+                            <label for="startdate">Arrival:</label>
+                            <input type="date" id="startdate" name="startdate" min="2025-01-01" max="2025-01-31" required>
 
-                    <label for="enddate">Departure:</label>
-                    <input type="date" id="enddate" name="enddate" min="2025-01-01" max="2025-01-31" required>
-                </div> <!-- .arrival-departure -->
+                            <label for="enddate">Departure:</label>
+                            <input type="date" id="enddate" name="enddate" min="2025-01-01" max="2025-01-31" required>
+                        </div> <!-- .arrival-departure -->
 
-                <div class="room-selection">
-                    <label for="room">Select room type:</label>
-                    <select id="room" name="room" required>
-                        <option value="" disabled selected>Room Type</option>
-                        <option value="1" data-price="1">Economy $1/day</option>
-                        <option value="2" data-price="2">Standard $2/day</option>
-                        <option value="3" data-price="4">Luxury $4/day</option>
-                    </select>
+                        <div class="room-selection">
+                            <label for="room">Select room type:</label>
+                            <select id="room" name="room" required>
+                                <option value="" disabled selected>Room Type</option>
+                                <option value="1" data-price="1">Economy $1/day</option>
+                                <option value="2" data-price="2">Standard $2/day</option>
+                                <option value="3" data-price="4">Luxury $4/day</option>
+                            </select>
 
-                    <div class="features-select">
-                        <input type="checkbox" id="bathtub" name="features-options[]" value="1" data-price="1">
-                        <label for="bathtub">Bathtub $1</label>
+                            <div class="features-select">
+                                <input type="checkbox" id="bathtub" name="features-options[]" value="1" data-price="1">
+                                <label for="bathtub">Bathtub $1</label>
 
-                        <input type="checkbox" id="pinball-game" name="features-options[]" value="2" data-price="2">
-                        <label for="pinball-game">Pinball Game $2</label>
+                                <input type="checkbox" id="pinball-game" name="features-options[]" value="2" data-price="2">
+                                <label for="pinball-game">Pinball Game $2</label>
 
-                        <input type="checkbox" id="sauna" name="features-options[]" value="3" data-price="3">
-                        <label for="sauna">Sauna $3</label>
-                    </div> <!-- .features-select -->
-                </div> <!-- .room-selection -->
+                                <input type="checkbox" id="sauna" name="features-options[]" value="3" data-price="3">
+                                <label for="sauna">Sauna $3</label>
+                            </div> <!-- .features-select -->
+                        </div> <!-- .room-selection -->
 
-                <div class="booker">
-                    <label for="firstname">First name:</label>
-                    <input type="text" id="firstname" name="firstname" required>
+                        <div class="booker">
+                            <label for="firstname">First name:</label>
+                            <input type="text" id="firstname" name="firstname" required>
 
-                    <label for="lastname">Last name:</label>
-                    <input type="text" id="lastname" name="lastname" required>
+                            <label for="lastname">Last name:</label>
+                            <input type="text" id="lastname" name="lastname" required>
 
-                    <label for="transferCode">Transfer Code:</label>
-                    <input type="text" id="transferCode" name="transferCode" required>
-                </div> <!-- .booker -->
+                            <label for="transferCode">Transfer Code:</label>
+                            <input type="text" id="transferCode" name="transferCode" required>
+                        </div> <!-- .booker -->
 
-                <div class="price">
-                    <div>
-                        <p>Room: </p>
-                        <p class="room-price">$0</p>
-                    </div>
+                        <div class="price">
+                            <div>
+                                <p>Room: </p>
+                                <p class="room-price">$0</p>
+                            </div>
 
-                    <div>
-                        <p>Features: </p>
-                        <p class="features-price">$0</p>
-                    </div>
+                            <div>
+                                <p>Features: </p>
+                                <p class="features-price">$0</p>
+                            </div>
 
-                    <div>
-                        <p>Total: </p>
-                        <p class="total-price">$0</p>
-                    </div>
-                </div> <!-- .price -->
+                            <div>
+                                <p>Total: </p>
+                                <p class="total-price">$0</p>
+                            </div>
+                        </div> <!-- .price -->
 
-                <div class="button-container">
-                    <button type="submit" class="eight-bit-btn btn-primary">Submit</button>
-                    <button type="reset" class="eight-bit-btn btn-clear">Clear</button>
-                </div> <!-- .button-container -->
-            </form>
-            <div class=" user-feedback">
-            </div> <!-- .user-feedback -->
+                        <div class="button-container">
+                            <button type="submit" class="eight-bit-btn btn-primary">Submit</button>
+                            <button type="reset" class="eight-bit-btn btn-clear">Clear</button>
+                        </div> <!-- .button-container -->
+                    </form>
+                    <div class=" user-feedback">
+                    </div> <!-- .user-feedback -->
+                </div> <!-- .booking-form-container -->
+            </div> <!-- .booking-grid -->
         </div> <!-- .inner-wrapper -->
     </section> <!-- #booking -->
 </main>
