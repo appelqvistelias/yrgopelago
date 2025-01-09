022bc3e8ec645ed7629bf0230b91e75b9cf0b575
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-01-09 14:50:23 +0100

    added session and login

--- init.php
+++ init.php
@@ -6,5 +6,10 @@ ini_set('display_errors', '0'); // Disable display errors
 ini_set('log_errors', '1'); // Enable error logging
 ini_set('error_log', __DIR__ . '/error.log'); // Set error log file
 
+session_start();
+
 require_once __DIR__ . "/vendor/autoload.php";
 require_once __DIR__ . "/functions.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
