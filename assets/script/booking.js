278d4dd7548654d2691c5644573d9ef199321511
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-01-23 16:01:17 +0100

    changed redirect pathing

--- assets/script/booking.js
+++ assets/script/booking.js
@@ -270,7 +270,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     greetings: result.data['additional_info']['greetings'],
                     gif_url: result.data['additional_info']['gif_url']
                 });
-                window.location.href = `../../src/view/successful_booking.php?${params.toString()}`;
+                window.location.href = `src/view/successful_booking.php?${params.toString()}`;
             } else {
                 errorFeedback.style.display = 'inline-block';
                 const errorMessage = createFeedbackElement(`Error: ${result.message}`, 'p');
