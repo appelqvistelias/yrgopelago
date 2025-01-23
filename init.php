69e70c3e21288f56397caeec508cfc2fc7eac927
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-01-23 15:10:48 +0100

    added base url

--- init.php
+++ init.php
@@ -6,6 +6,8 @@ ini_set('display_errors', '0'); // Disable display errors
 ini_set('log_errors', '1'); // Enable error logging
 ini_set('error_log', __DIR__ . '/error.log'); // Set error log file
 
+define('BASE_URL', 'http://www.eappqlevist.se');
+
 session_start();
 
 require_once __DIR__ . "/vendor/autoload.php";
